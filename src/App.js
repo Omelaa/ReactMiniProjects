37133a616aa6757865f27d9ecbbc0dcfccaa74cd
Omelaa <ostpa28@gmail.com>
2023-01-05 14:13:38 +0200

    Counter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import './index.scss';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
-      </div>
-    </div>
-  );
+    const [count, setCount] = useState(0);
+
+    const isInc = () => {
+        setCount((prev) => prev + 1);
+    };
+
+    const isDec = () => {
+        setCount((prev) => prev - 1);
+    };
+
+    return (
+        <div className="App">
+            <div>
+                <h2>Лічильник:</h2>
+                <h1>{count}</h1>
+                <button className={count === 0 ? "minus minus__disabled" : "minus"} onClick={isDec}
+                        disabled={count === 0}>- Мінус
+                </button>
+                <button className="plus" onClick={isInc}>Плюс +</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
