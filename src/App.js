af838eefe6b73b0f5d36698dd308f2ef443a4753
Omelaa <ostpa28@gmail.com>
2023-01-05 14:20:19 +0200

    update

--- src/App.js
+++ src/App.js
@@ -15,12 +15,12 @@ function App() {
     return (
         <div className="App">
             <div>
-                <h2>Лічильник:</h2>
+                <h2>Counter:</h2>
                 <h1>{count}</h1>
                 <button className={count === 0 ? "minus minus__disabled" : "minus"} onClick={isDec}
-                        disabled={count === 0}>- Мінус
+                        disabled={count === 0}>- Minus
                 </button>
-                <button className="plus" onClick={isInc}>Плюс +</button>
+                <button className="plus" onClick={isInc}>Plus +</button>
             </div>
         </div>
     );
